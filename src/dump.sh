8db1bfb84a988efad70212924182f3e6dcbf035d
Andreas Kasper <djassi@web.de>
2020-05-05 07:28:02 +0200

    nice text formatting

--- src/dump.sh
+++ src/dump.sh
@@ -9,21 +9,21 @@ IGNORE_DATABASE=${IGNORE_DATABASE}
 
 
 if [[ ${DB_USER} == "" ]]; then
-	echo "Missing DB_USER env variable"
+	echo "[\e[91mERROR\e[0m] Missing DB_USER env variable"
 	exit 1
 fi
 if [[ ${DB_PASS} == "" ]]; then
-	echo "Missing DB_PASS env variable"
+	echo "[\e[91mERROR\e[0m] Missing DB_PASS env variable"
 	exit 1
 fi
 if [[ ${DB_HOST} == "" ]]; then
-	echo "Missing DB_HOST env variable"
+	echo "[\e[91mERROR\e[0m] Missing DB_HOST env variable"
 	exit 1
 fi
 
 if [[ ${ALL_DATABASES} == "" ]]; then
 	if [[ ${DB_NAME} == "" ]]; then
-		echo "Missing DB_NAME env variable"
+		echo "[\e[91mERROR\e[0m] Missing DB_NAME env variable"
 		exit 1
 	fi
 	mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" "$@" "${DB_NAME}" > /mysqldump/"${DB_NAME}".sql
@@ -31,8 +31,9 @@ else
 	databases=`mysql --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
 for db in $databases; do
     if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] && [[ "$db" != "$IGNORE_DATABASE" ]]; then
-        echo "Dumping database: $db"
+        echo -ne "[*] Dumping database: $db "
         mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --databases $db > /mysqldump/$db.sql
+	echo -e "\e[92mdone\e[0m"
     fi
 done
 fi
